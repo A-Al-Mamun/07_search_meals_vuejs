b9033bd943ec3d4737b05e027bb7bc3b9989e2bb
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-02-08 22:19:08 +0600

    Tailwind css add but not work

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
